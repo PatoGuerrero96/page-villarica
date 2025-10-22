c010a6e81a89009115a0b966605ea21331645ff3
Patricio Guerrero <gpatricio515@gmail.com>
2025-10-21 21:31:50 -0300

    Version con Headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "no-referrer-when-downgrade" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" },
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self' https: data:;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https:; 
+              style-src 'self' 'unsafe-inline' https:; 
+              img-src 'self' data: https:;
+              font-src 'self' https:;
+              connect-src 'self' https:;
+            `.replace(/\s+/g, ' ')
+          },
+          { key: "Strict-Transport-Security", value: "max-age=63072000; includeSubDomains; preload" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
